1a04f11438b4a1b2c5f85c7b21d60f771c38e910
Ricqua <ricqua@gmail.com>
2021-02-18 12:15:23 +0900

    click reveal function added

--- cell.js
+++ cell.js
@@ -4,6 +4,7 @@ function Cell(x, y, w) {
   this.x = x;
   this.y = y;
   this.w = w;
+  this.robot = false;
 
   if (sumCoordinates(x, y) >= dangerZone) {
     this.cellStatus = false;
@@ -19,15 +20,40 @@ function sumCoordinates(i, j) {
   return i + j;
 }
 
+Cell.prototype.maxReachArea = function () {
+  var countReachable = 0;
+  if (this.cellStatus) {
+    return -1;
+  }
+};
+
 Cell.prototype.show = function () {
   // stroke("white");
   noStroke();
   noFill();
   rect(this.x, this.y, this.w, this.w);
+  // fill(0);
+  // textAlign(CENTER);
+  // text(this.x);
 
   if (this.cellStatus === true) {
     // stroke("green");
-    fill("green");
+    fill(125);
     rect(this.x, this.y, this.w, this.w);
+    if (this.robot) {
+      fill("green");
+      rect(this.x, this.y, this.w, this.w);
+      // ellipse(this.x + this.w / 2, this.y + this.w / 2, this.w + 0.5);
+    }
+
+    // fillText("x", this.x / 2, this.y / 2);
   }
+};
+
+Cell.prototype.contains = function (x, y) {
+  return x > this.x && x < this.x + this.w && y > this.y && y < this.y + this.w;
+};
+
+Cell.prototype.reveal = function () {
+  this.robot = true;
 };
